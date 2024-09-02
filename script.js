5ba402aac5e63df329d5b4c6744e9d97f35a8503
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-09-02 09:56:55 +0200

    added comments to script

--- script.js
+++ script.js
@@ -4,17 +4,23 @@ function validateSyntax() {
     const prefix = "pet_";
     const dateLength = 4; 
    
+    // strictly starts with "pet_"
     if (!input.startsWith(prefix)) {
         result = "Invalid Syntax";
     } else {
+        // (4, 8) : extracts from index 4 - 7
         let petDateStr = input.substring(prefix.length, prefix.length + dateLength);
+        // converts petDateStr's value into a number
         let petDateNum = Number(petDateStr);
 
+        // checks if its a valid number, has 4 characters and if string ONLY had DIGITS
         if (isNaN(petDateNum) || petDateStr.length !== dateLength || !/^\d+$/.test(petDateStr)) {
             result = "Invalid Syntax";
         } else {
+            // extracting the pet name AFTER date validation
             let petName = input.substring(prefix.length + dateLength);
         
+            // pet name needs to be at least ONE character
             if (petName.length < 1) {
                 result = "Invalid Syntax";
             } else {
@@ -23,7 +29,6 @@ function validateSyntax() {
         }
     }
 
-    
     document.getElementById('result').innerText = result;
 }
 
